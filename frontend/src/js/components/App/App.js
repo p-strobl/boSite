6e4f315739b93f9a5daac2def35c0eefe1cbd88f
P. Strobl <p.str.hh@icloud.com>
2019-07-10 19:47:36 +0200

    Update eslint

--- frontend/src/js/components/App/App.js
+++ frontend/src/js/components/App/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from './App.scss';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./App.scss";
 
 export default function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState({
-    message: '',
+    message: "",
     state: false,
   });
 
-  const url = 'http://localhost:5000/recipes';
+  const url = "http://localhost:5000/recipes";
 
   useEffect(() => {
     axios
       .get(url)
       .then((result) => {
-        console.log('result', result);
+        console.log("result", result);
         setData(result);
         setLoading(false);
       })
@@ -29,13 +29,13 @@ export default function App() {
   }, []);
 
   const handleClick = (event) => {
-    console.log('Fetched', error.state);
+    console.log("Fetched", error.state);
   };
 
   return (
     <>
       <h1 className={styles.bla}>Fetched Data</h1>
-      {loading ? <p>...is loading</p> : ''}
+      {loading ? <p>...is loading</p> : ""}
       <div>
         {/* {data.map((item) => { */}
         {/*  return <p key={item._id}>{item.name}</p>; */}
