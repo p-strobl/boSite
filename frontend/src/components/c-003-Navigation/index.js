30a802f5168b9df8088b1e8222825b381a9afffe
P. Strobl <p.str.hh@icloud.com>
2019-07-16 00:09:25 +0200

    Started routing

--- frontend/src/components/c-003-Navigation/index.js
+++ frontend/src/components/c-003-Navigation/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import uuidv4 from "uuid/v4";
+import { A } from "hookrouter";
+import React, { useContext } from "react";
 import RouterContext from "../../context/RouteContext";
 
 export default function HeadNavigation() {
-
-  console.log("rr", RouterContext);
-
-  return (
-    <div>Navi</div>
-  );
+  const routes = useContext(RouterContext);
+  return Object.entries(routes).map(([key, value]) => {
+    return (
+      <ul key={uuidv4()}>
+        <li key={uuidv4()}>
+          <A key={uuidv4()} href={value}>
+            {key}
+          </A>
+        </li>
+      </ul>
+    );
+  });
 }
