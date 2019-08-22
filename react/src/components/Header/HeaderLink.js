b411885489fc46e7e4eca192ebdf52fd35041557
P. Strobl <p.str.hh@icloud.com>
2019-08-22 23:11:55 +0200

    Link icons added

--- react/src/components/Header/HeaderLink.js
+++ react/src/components/Header/HeaderLink.js
@@ -2,7 +2,9 @@ import uuidv4 from "uuid/v4";
 import React, { useContext } from "react";
 import Class from "classnames";
 import { A } from "hookrouter";
+
 import RouterContext from "~context/RouteContext";
+import { Images } from "~assets/images";
 
 import "./HeaderLink.scss";
 
@@ -10,13 +12,21 @@ export function HeaderLinkList() {
   const routes = useContext(RouterContext);
   const links = Object.entries(routes).map(([key, value]) => {
     return (
-      <A
-        className={Class(`link link__${key}`)}
-        key={uuidv4()}
-        href={value}
-        title={`Back to ${key}`}>
-        {key}
-      </A>
+      <div className={Class("link-wrapper")} key={uuidv4()}>
+        <A
+          className={Class(`link link__${key}`)}
+          key={uuidv4()}
+          href={value}
+          title={`Back to ${key}`}>
+          <img
+            className={Class("link__image")}
+            src={Images[key]}
+            alt={`${key} icon`}
+            key={uuidv4()}
+          />
+          <div>{key}</div>
+        </A>
+      </div>
     );
   });
 
