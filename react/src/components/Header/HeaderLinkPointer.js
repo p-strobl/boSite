92492b962303a54e21cdd954dc6395aaf87e602a
P. Strobl <p.str.hh@icloud.com>
2019-09-11 00:08:29 +0200

    Arrow Classname

--- react/src/components/Header/HeaderLinkPointer.js
+++ react/src/components/Header/HeaderLinkPointer.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { usePath } from "hookrouter";
 import Class from "classnames";
+import * as Utils from "~src/helper/Utilities";
 
 import "./HeaderLinkPointer.scss";
+import RouterContext from "~context/RouteContext";
 
 export function HeaderLinkPointer() {
-  return <div className={Class("HeaderLinkPointer")} />;
+  const routes = useContext(RouterContext);
+  const Path = usePath();
+  let arrowClass = "";
+
+  Object.entries(routes).forEach(([key, value]) => {
+    if (value === Path) {
+      arrowClass = key;
+    }
+  });
+
+  return (
+    <div className={Class("Header__LinkPointerWrapper")}>
+      <span
+        className={Class(
+          `Header__LinkPointerArrow Header__LinkPointerArrow--${Utils.capitalizeFirstLetter(
+            arrowClass,
+          )}`,
+        )}
+      />
+    </div>
+  );
 }
