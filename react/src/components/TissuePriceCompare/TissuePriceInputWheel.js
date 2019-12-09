c13a4026e40f02c739e496919fc32524621fca2a
P. Strobl <p.strobl.hh@gmail.com>
2019-12-09 18:31:58 +0100

    Update TissuePriceInputWheel.js

--- react/src/components/TissuePriceCompare/TissuePriceInputWheel.js
+++ react/src/components/TissuePriceCompare/TissuePriceInputWheel.js
@@ -1,24 +1,52 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import Class from "classnames";
 import PropTypes from "prop-types";
 import uuidv4 from "uuid/v4";
 
 import "./TissuePriceInputWheel.scss";
+import { TissuePriceCalculatorContext } from "~context/TissuePriceCalculatorContext";
 
-export class TissuePriceInputWheel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      wheelRange: [],
-    };
-  }
+export const TissuePriceInputWheel = ({ context, range }) => {
+  // const numbersOnly = (element, inputValue) => {
+  //   const numberRegex = /[^0-9,]/g;
+  //
+  //   if (numberRegex.test(inputValue)) {
+  //     element.target.value = inputValue.replace(numberRegex, "");
+  //     return false;
+  //   }
+  //   return true;
+  // };
 
-  componentDidMount() {
-    const { range } = this.props;
+  // const validatePrice = (element) => {
+  //   const inputValue = element.target.value;
+  //
+  //   if (inputValue.length === 0) return false;
+  //
+  //   const inputIsValid = numbersOnly(element, inputValue);
+  //
+  //   if (inputIsValid) {
+  //     const digitRegex = /\D/g;
+  //     const decimalCommaRegex = /\B(?=(\d{2})(?!\d))/g;
+  //
+  //     element.target.value = inputValue.replace(digitRegex, "").replace(decimalCommaRegex, ",");
+  //
+  //     return parseFloat(inputValue);
+  //   }
+  //   return false;
+  // };
 
+  const {
+    [context]: [input, setInput],
+  } = useContext(TissuePriceCalculatorContext);
+
+  // const validateInputPrice = (clickedElement) => {
+  //   setInput(validatePrice(clickedElement));
+  // };
+
+  const createWheelRangeElements = () => {
     const wheelRange = [];
 
-    for (let i = 1; i < range + 1; i++) {
+    for (let i = 1; i < range + 1; i += 1) {
       wheelRange.push(
         <div className="TissueInputWheel__Number" key={uuidv4()}>
           {i}
@@ -26,41 +54,65 @@ export class TissuePriceInputWheel extends Component {
       );
     }
 
-    this.setState(() => {
-      return {
-        wheelRange,
-      };
-    });
-  }
+    return wheelRange;
+  };
 
-  // componentWillUnmount() {
-  // };
+  const getWheelInput = (element) => {
+    console.log("", element.target.value);
+  };
 
-  render() {
-    const { wheelRange } = this.state;
+  return (
+    <div className={Class("TissueInputWheel")} key={uuidv4()} onWheel={getWheelInput}>
+      <div className="TissueInputWheel__Button TissueInputWheel__Button--Up" />
+      <div className="TissueInputWheel__Container">{createWheelRangeElements() || ""}</div>
+      <div className="TissueInputWheel__Button TissueInputWheel__Button--Down" />
+    </div>
+  );
+};
 
-    // const wheelRange = [];
-
-    // for (let i = 1; i < range; i++) {
-    //   wheelRange.push(
-    //     <div className="TissueInputWheel__Number" key={uuidv4()}>
-    //       {i}
-    //     </div>,
-    //   );
-    // }
-
-    return (
-      <div className={Class("TissueInputWheel")} key={uuidv4()}>
-        <div className="TissueInputWheel__Container">{wheelRange || ""}</div>
-      </div>
-    );
-  }
-}
+// export class TissuePriceInputWheel extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//
+//   componentDidMount() {}
+//
+//   createWheelRangeElements = () => {
+//     const { range } = this.props;
+//     const wheelRange = [];
+//
+//     for (let i = 1; i < range + 1; i += 1) {
+//       wheelRange.push(
+//         <div className="TissueInputWheel__Number" key={uuidv4()}>
+//           {i}
+//         </div>,
+//       );
+//     }
+//
+//     return wheelRange;
+//   };
+//
+//   // componentWillUnmount() {
+//   // };
+//
+//   render() {
+//     const wheelRange = this.createWheelRangeElements();
+//
+//     return (
+//       <div className={Class("TissueInputWheel")} key={uuidv4()}>
+//         <div className="TissueInputWheel__Container">{wheelRange || ""}</div>
+//       </div>
+//     );
+//   }
+// }
 
 TissuePriceInputWheel.defaultProps = {
+  context: "",
   range: 0,
 };
 
 TissuePriceInputWheel.propTypes = {
+  context: PropTypes.string,
   range: PropTypes.number,
 };
