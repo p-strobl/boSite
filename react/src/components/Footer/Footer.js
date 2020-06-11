9928c93a3211adf5d94421e499cee6c14755e2ab
P. Strobl <p.strobl.hh@gmail.com>
2020-06-11 02:36:38 +0200

    Footer uiClasses added

--- react/src/components/Footer/Footer.js
+++ react/src/components/Footer/Footer.js
@@ -1,19 +1,25 @@
 import React from "react";
-import Class from "classnames";
 
 import "./Footer.scss";
 import { Emoji } from "~components/Emoji";
 
 export function Footer() {
+  const uiClasses = {
+    footer: "Footer",
+    footerContainer: "Footer__Container",
+    footerText: "Footer__Text",
+    emojiHeart: "Emoji__Heart",
+  };
+
   const date = new Date();
   const currentYear = date.getFullYear();
-  
+
   return (
-    <footer className={Class("Footer")}>
-      <div className={Class("Footer__Container")}>
-        <div className={Class("Footer__Text")}>
+    <footer className={uiClasses.footer}>
+      <div className={uiClasses.footerContainer}>
+        <div className={uiClasses.footerText}>
           boSite
-          <Emoji classs="Footer__" emojiClass="Emoji__Heart" label="heart" />
+          <Emoji classs={`${uiClasses.footer}__`} emojiClass={uiClasses.emojiHeart} label="heart" />
           {currentYear}
         </div>
       </div>
