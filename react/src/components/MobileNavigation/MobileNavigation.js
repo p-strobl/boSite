b7181d962f9d945d1ca507a219b4267fb399e488
P. Strobl <p.strobl.hh@gmail.com>
2020-06-12 02:56:58 +0200

    MobileNavigation uiClasses added

--- react/src/components/MobileNavigation/MobileNavigation.js
+++ react/src/components/MobileNavigation/MobileNavigation.js
@@ -4,29 +4,39 @@ import "./MobileNavigation.scss";
 import { NavigationLinkList } from "~components/Navigation/NavigationLinkList";
 
 export function MobileNavigation() {
+  const uiClasses = {
+    navigation: "MobileNavigation",
+    wrapper: "MobileNavigation__Wrapper",
+    toggleButtonWrapper: "MobileNavigation__ToggleButtonWrapper",
+    toggleButton: "MobileNavigation__ToggleButton",
+    buttonOpen: "MobileNavigation__ToggleButton--Open",
+    linkList: "MobileNavigation__LinkList",
+    linkListActive: "MobileNavigation__LinkList--Active",
+  };
+
   const toggleButtonHandler = (event) => {
-    if (event.target.type !== "button") return false;
+    if (event.target.type !== "button") return;
 
     const navigationButton = event.target;
-    const navigationWrapper = event.target.closest(".MobileNavigation__Wrapper");
-    const navigationLinkList = navigationWrapper.querySelector(".MobileNavigation__LinkList");
+    const navigationWrapper = event.target.closest(`.${uiClasses.wrapper}`);
+    const navigationLinkList = navigationWrapper.querySelector(`.${uiClasses.linkList}`);
 
-    navigationButton.classList.toggle("MobileNavigation__ToggleButton--Open");
-    navigationLinkList.classList.toggle("MobileNavigation__LinkList--Active");
+    navigationButton.classList.toggle(`${uiClasses.buttonOpen}`);
+    navigationLinkList.classList.toggle(`${uiClasses.linkListActive}`);
   };
 
   return (
-    <nav className="MobileNavigation">
-      <div className="MobileNavigation__Wrapper">
-        <div className="MobileNavigation__ToggleButtonWrapper">
+    <nav className={uiClasses.navigation}>
+      <div className={uiClasses.wrapper}>
+        <div className={uiClasses.toggleButtonWrapper}>
           <button
             aria-label="Mobile Menu Button"
-            className="MobileNavigation__ToggleButton"
+            className={uiClasses.toggleButton}
             onClick={toggleButtonHandler}
             type="button"
           />
         </div>
-        <div className="MobileNavigation__LinkList">
+        <div className={uiClasses.linkList}>
           <NavigationLinkList />
         </div>
       </div>
